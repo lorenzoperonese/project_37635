45ae8c3fc7e7a7b5ff28a42c8aa4a69fa7bcdf61
Lorenzo Peronese <lorenzo.peronese@studio.unibo.it>
2023-07-15 20:10:30 +0200

    Maybe I have understood how it works!!!

---
+++ connectx/OurPlayer/OurPlayer.java
@@ -0,0 +1,19 @@
+package connectx.OurPlayer;
+
+import connectx.CXPlayer;
+import connectx.CXBoard;
+
+public class OurPlayer implements CXPlayer{
+    public OurPlayer(){}
+    public void initPlayer(int M, int N, int K,  boolean first, int timeout_in_secs) {}
+
+    public int selectColumn(CXBoard B) {
+        //algorithm
+        return 0;
+    }
+
+    public String playerName() {
+        return "OurPlayer";
+    }
+}
+
