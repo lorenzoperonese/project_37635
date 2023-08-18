57f4c9dc535020ff1c4983f81eaf3134072a68e3
Lorenzo Peronese <lorenzo.peronese@studio.unibo.it>
2023-08-18 10:04:34 +0200

    Added score_depth to L6

---
+++ connectx/L6/Score_Depth.java
@@ -0,0 +1,16 @@
+package connectx.L6;
+
+public class Score_Depth {
+    private int score;
+    private int depth;
+    public Score_Depth(int score, int depth) {
+        this.score = score;
+        this.depth = depth;
+    }
+    public int getScore() {
+        return score;
+    }
+    public int getDepth() {
+        return depth;
+    }
+}
