252a3e69e828d9a754b9f3b593fc90f4171fcc8c
Lorenzo Peronese <lorenzo.peronese@studio.unibo.it>
2023-08-26 12:05:21 +0200

    Typos

--- connectx.sh
+++ connectx.sh
@@ -14,16 +14,16 @@ output_file2="2.out"
 for parametro in "${parametri[@]}"; do
     echo "$parametro" >> "$output_file1"
     echo "$parametro" >> "$output_file2"
-    java -cp "/home/students/lorenzo.peronese/ConnectX/project_37635" connectx.CXPlayerTester $parametro connectx.L6.L6 connectx.MxLxPlayer.MxLxPlayer -v -r 100 >> "$output_file1"
-    java -cp "/home/students/lorenzo.peronese/ConnectX/project_37635" connectx.CXPlayerTester $parametro connectx.MxLxPlayer.MxLxPlayer connectx.L6.L6 -v -r 100 >> "$output_file2"
+    java -cp "/home/students/lorenzo.peronese/ConnectX/project_37635" connectx.CXPlayerTester $parametro connectx.L6.L6 connectx.L5.L5 -v -r 100 >> "$output_file1"
+    java -cp "/home/students/lorenzo.peronese/ConnectX/project_37635" connectx.CXPlayerTester $parametro connectx.L5.L5 connectx.L6.L6 -v -r 100 >> "$output_file2"
     echo "--------------------------------------" >> "$output_file1"
     echo "--------------------------------------" >> "$output_file2"
 done
 for parametro in "${parametri_grossi[@]}"; do
     echo "$parametro" >> "$output_file1"
     echo "$parametro" >> "$output_file2"
-    java -cp "/home/students/lorenzo.peronese/ConnectX/project_37635" connectx.CXPlayerTester $parametro connectx.L6.L6 connectx.MxLxPlayer.MxLxPlayer -v -r 25 >> "$output_file1"
-    java -cp "/home/students/lorenzo.peronese/ConnectX/project_37635" connectx.CXPlayerTester $parametro connectx.MxLxPlayer.MxLxPlayer connectx.L6.L6 -v -r 25 >> "$output_file2"
+    java -cp "/home/students/lorenzo.peronese/ConnectX/project_37635" connectx.CXPlayerTester $parametro connectx.L6.L6 connectx.L5.L5 -v -r 25 >> "$output_file1"
+    java -cp "/home/students/lorenzo.peronese/ConnectX/project_37635" connectx.CXPlayerTester $parametro connectx.L5.L5 connectx.L6.L6 -v -r 25 >> "$output_file2"
     echo "--------------------------------------" >> "$output_file1"
     echo "--------------------------------------" >> "$output_file2"
 done
